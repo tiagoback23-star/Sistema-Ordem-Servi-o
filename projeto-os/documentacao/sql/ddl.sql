23853595748b35470285748bc6c99381a4db72d0
tiagoback23-star <tiagoback23@gmail.com>
2025-12-02 21:04:36 -0300

    Update ddl.sql

---
+++ projeto-os/documentacao/sql/ddl.sql
@@ -0,0 +1,13 @@
+CREATE TABLE usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE produtos (
+    id_produto SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    quantidade INTEGER NOT NULL
+);
